64cc5d65ca91f0f6370ca46648c298b0e6bc4f7d
bioastroiner <68257484+bioastroiner@users.noreply.github.com>
2024-01-15 15:54:37 +0330

    * fix Hook checking for cancel condition

--- src/main/java/bioast/mods/gt6mapper/hooks/GTHooks.java
+++ src/main/java/bioast/mods/gt6mapper/hooks/GTHooks.java
@@ -3,6 +3,7 @@ package bioast.mods.gt6mapper.hooks;
 import cpw.mods.fml.common.eventhandler.Event;
 import gregapi.random.IHasWorldAndCoords;
 import gregapi.recipes.Recipe;
+import gregapi.recipes.maps.RecipeMapPrinter;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fluids.FluidStack;
@@ -12,11 +13,12 @@ import java.util.function.Supplier;
 
 public class GTHooks {
 	public static Recipe onFindRecipe(Recipe.RecipeMap aMap, IHasWorldAndCoords aTileEntity, Recipe aRecipe,/*to make ASM happy & change less code as possible*/boolean aLoop, boolean aNotUnificated, long aSize, ItemStack aSpecialSlot, FluidStack[] aFluids, ItemStack... aInputs) {
+		Recipe rRecipe = aMap.findRecipeInternal(aTileEntity, aRecipe, aLoop, aNotUnificated, aSize, aSpecialSlot, aFluids, aInputs);
 		OnFindGTRecipeEvent event = new OnFindGTRecipeEvent(aMap, aRecipe, aNotUnificated, aTileEntity, aSize, aSpecialSlot, aInputs, aFluids);
-		if (MinecraftForge.EVENT_BUS.post(event) && event.aRecipeConsumer.get() != null)
+		MinecraftForge.EVENT_BUS.post(event)
+		if (aRecipe == null && event.aRecipeConsumer.get() != null)
 			return event.aRecipeConsumer.get();
-		return aMap.findRecipeInternal(aTileEntity, aRecipe, aLoop, aNotUnificated, aSize, aSpecialSlot, aFluids, aInputs);
-
+		return rRecipe;
 	}
 
 	/**
@@ -24,6 +26,13 @@ public class GTHooks {
 	 * This recipes are checked when a machine detects items in its inventory
 	 * Here you tell it to run a recipe directly without registering
 	 * of course it means this recipes are not seen in NEI so you need to add a Fake one for NEI
+	 * It also won't let you override if a preDefined recipe is already found
+	 *
+	 * @author BioAstroiner
+	 * @see cpw.mods.fml.common.eventhandler.Event
+	 * @see RecipeMapPrinter
+	 * <p>
+	 * Set your Recipe Return value to this event's accept function
 	 */
 	public static class OnFindGTRecipeEvent extends Event implements Consumer<Recipe> {
 		public Recipe.RecipeMap aRecipeMap;
